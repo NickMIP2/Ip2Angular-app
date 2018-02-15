4a4daa56359dcd409e3d0ffda007407563c776b5
Dean Aerts <deanaerts@users.noreply.github.com>
2018-02-15 18:18:49 +0100

    tokens aangepast om overeen te komen met backend

--- src/app/services/auth.constant.ts
+++ src/app/services/auth.constant.ts
@@ -1,3 +1,3 @@
-export const TOKEN_AUTH_USERNAME = 'testkandoe';
-export const TOKEN_AUTH_PASSWORD = 'kandoe123';
+export const TOKEN_AUTH_USERNAME = 'authkandoeclientid';
+export const TOKEN_AUTH_PASSWORD = 'authkandoesecret';
 export const TOKEN_NAME = 'access_token';
