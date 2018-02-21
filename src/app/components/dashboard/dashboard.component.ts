035a93b273fa587265416e8d8353c27d11c01522
bartmeeus <bart.meeus@student.kdg.be>
2018-02-21 12:30:09 +0100

    merge conflict fix

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 
-<<<<<<< HEAD
 import {Title} from '@angular/platform-browser';
 
-======
-=
->>>>>>>
-cb6e67564a345bf37e6e1b7087b6ec2f85d2b766;
 
 @Component({
   selector: 'app-dashboard',
