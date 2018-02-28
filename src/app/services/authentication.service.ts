a18336721faca6364f91025061be0615004b06e3
nickmarcoen <nick.marcoen@student.kdg.be>
2018-02-28 09:40:38 +0100

    quick fix

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -12,10 +12,10 @@ export class AuthenticationService {
   constructor(private http: HttpClient) {
   }
 
-
-
-  login(username: string, password: string): Observable<any> {
+  login(username: string, password: string): Observable<any>{
     const credentials = {username: username, password: password};
+    const testregi = {username: username, password: password, firstname: 'nickjwt', lastname: 'marcoenjwt', email: 'testmail@nick.com'};
+    this.http.post("https://kandoe-backend.herokuapp.com/register", testregi, httpOptions); // test
     return this.http.post('https://kandoe-backend.herokuapp.com/token/generate-token', credentials, httpOptions);
   }
 
