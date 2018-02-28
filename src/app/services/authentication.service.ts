729db37e247c5c21bb1c59dd0ed183b3431fc919
nickmarcoen <nick.marcoen@student.kdg.be>
2018-02-28 09:54:20 +0100

    header

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -2,10 +2,6 @@ import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-};
-
 @Injectable()
 export class AuthenticationService {
 
@@ -15,8 +11,8 @@ export class AuthenticationService {
   login(username: string, password: string): Observable<any>{
     const credentials = {username: username, password: password};
     const testregi = {username: username, password: password, firstname: 'nickjwt', lastname: 'marcoenjwt', email: 'testmail@nick.com'};
-    this.http.post("https://kandoe-backend.herokuapp.com/register", testregi, httpOptions); // test
-    return this.http.post('https://kandoe-backend.herokuapp.com/token/generate-token', credentials, httpOptions);
+    this.http.post("https://kandoe-backend.herokuapp.com/register", testregi); // test
+    return this.http.post('https://kandoe-backend.herokuapp.com/token/generate-token', credentials);
   }
 
 }
