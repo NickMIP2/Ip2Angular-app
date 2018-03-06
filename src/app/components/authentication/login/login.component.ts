ee35b7de021b2b651906ae5a5e8154565e736cc2
nickmarcoen <nick.marcoen@student.kdg.be>
2018-03-06 15:22:55 +0100

    navbar fixed

--- src/app/components/authentication/login/login.component.ts
+++ src/app/components/authentication/login/login.component.ts
@@ -26,7 +26,7 @@ export class LoginComponent implements OnInit {
       data => {
         this.tokenStorage.saveToken(data.authToken);
         this.userIdStorage.saveUserId(data.userId);
-        this.router.navigate(['dashboard']);
+        this.router.navigate(['kandoe/dashboard']);
       },
       error => {
         console.error('Error logging in!');
