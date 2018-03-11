baf97912d88f653baec4636e4d5a817eea0e42b4
nickmarcoen <nick.marcoen@student.kdg.be>
2018-03-12 00:40:20 +0100

    themes klaar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,18 @@ import {RouterModule, Routes} from '@angular/router';
 import {DashboardComponent} from './components/kandoe/dashboard/dashboard.component';
 import {ThemesComponent} from './components/kandoe/themes/themes.component';
 import {NewThemeComponent} from './components/kandoe/new-theme/new-theme.component';
-import {ThemedetailCardsComponent} from './components/kandoe/themedetail/components/themedetail-cards/themedetail-cards.component';
-import {CardEditComponent} from './components/kandoe/themedetail/components/themedetail-cards/card-edit/card-edit.component';
+import {CardEditComponent} from './components/kandoe/themedetail/components/cards/card-edit/card-edit.component';
 import {ThemedetailOrganiserComponent} from './components/kandoe/themedetail/components/themedetail-organiser/themedetail-organiser.component';
 import {ThemedetailCategoriesComponent} from './components/kandoe/themedetail/components/themedetail-categories/themedetail-categories.component';
 import {ThemedetailOverviewComponent} from './components/kandoe/themedetail/components/themedetail-overview/themedetail-overview.component';
 import {ThemedetailComponent} from './components/kandoe/themedetail/themedetail.component';
-import {CardNewComponent} from './components/kandoe/themedetail/components/themedetail-cards/card-new/card-new.component';
+import {CardNewComponent} from './components/kandoe/themedetail/components/cards/card-new/card-new.component';
 import {LoginComponent} from './components/authentication/login/login.component';
 import {NewSessionComponent} from './components/kandoe/session/new-session/new-session.component';
 import {RegisterComponent} from './components/authentication/register/register.component';
 import {KandoeComponent} from './components/kandoe/kandoe.component';
 import {ChatComponent} from './components/kandoe/chat/chat.component';
+import {CardOverviewComponent} from './components/kandoe/themedetail/components/cards/card-overview/card-overview.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/login', pathMatch: 'full'},
@@ -34,11 +34,11 @@ const routes: Routes = [
         children: [
           {path: '', redirectTo: 'overview', pathMatch: 'full'},
           {path: 'overview', component: ThemedetailOverviewComponent},
-          {path: 'cards', component: ThemedetailCardsComponent},
-          {path: 'cards/card-new', component: CardNewComponent},
-          {path: 'cards/:cardId', component: CardEditComponent},
+          {path: 'categories/:categoryId/cards/new-card', component: CardNewComponent},
+          {path: 'categories/:categoryId/cards/:cardId/edit-card', component: CardEditComponent},
           {path: 'organisers', component: ThemedetailOrganiserComponent},
-          {path: 'categories', component: ThemedetailCategoriesComponent}
+          {path: 'categories', component: ThemedetailCategoriesComponent},
+          {path: 'categories/:categoryId/overview', component: CardOverviewComponent}
         ]
       }
     ]
--- src/app/services/card.service.ts
+++ src/app/services/card.service.ts
@@ -15,33 +15,29 @@ export class CardService {
   constructor(private http: HttpClient) {
   }
 
-  createCard(card: Card, themeId: number, userId: number): Observable<any> {
+  createCard(categoryId: number,card: Card, themeId: number, userId: number): Observable<any> {
     const body = JSON.stringify(card);
     console.log('theme id: ' + themeId);
     console.log('json body:' + body);
-    return this.http.post('https://kandoe-backend.herokuapp.com/users/' + userId + '/themes/' + themeId + '/cards', body, httpOptions);
+    return this.http.post('https://kandoe-backend.herokuapp.com/users/' + userId + '/themes/' + themeId + '/categories/' + categoryId + '/cards', body, httpOptions);
   }
 
-  updateCard(card: Card, themeId: number, userId: number): Observable<any> {
+  updateCard(categoryId: number, card: Card, themeId: number, userId: number): Observable<any> {
     const body = JSON.stringify(card);
     console.log('theme id: ' + themeId);
     console.log('json body:' + body);
-    return this.http.put('https://kandoe-backend.herokuapp.com/users/' + userId + '/themes/' + themeId + '/cards/' + card.id, body, httpOptions);
+    return this.http.put('https://kandoe-backend.herokuapp.com/users/' + userId + '/themes/' + themeId + '/categories/' + categoryId +  '/cards/' + card.id, body, httpOptions);
   }
 
-  deleteCard(id: number, userId: number, themeId: number): Observable<any> {
-    return this.http.delete('https://kandoe-backend.herokuapp.com/users/' + userId + '/themes/' + themeId + '/cards/' + id, httpOptions);
-  }
-
-  getCardsByTheme(themeId: number, userId: number): Observable<any> {
-    return this.http.get('https://kandoe-backend.herokuapp.com/users/' + userId + '/themes/' + themeId + '/cards');
+  deleteCard(categoryId: number,id: number, userId: number, themeId: number): Observable<any> {
+    return this.http.delete('https://kandoe-backend.herokuapp.com/users/' + userId + '/themes/' + themeId + '/categories/' + categoryId + '/cards/' + id, httpOptions);
   }
 
   getCardsByCategory(categoryId: number, themeId: number, userId: number): Observable<any> {
     return this.http.get('https://kandoe-backend.herokuapp.com/users/' + userId + '/themes/' + themeId + '/categories/' + categoryId + '/cards');
   }
 
-  getCard(cardId: number, themeId: number, userId: number): Observable<any> {
-    return this.http.get('https://kandoe-backend.herokuapp.com/users/' + userId + '/themes/' + themeId + '/cards/' + cardId);
+  getCard(categoryId: number,cardId: number, themeId: number, userId: number): Observable<any> {
+    return this.http.get('https://kandoe-backend.herokuapp.com/users/' + userId + '/themes/' + themeId + '/categories/' + categoryId + '/cards/' + cardId);
   }
 }
