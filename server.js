c50b073ca1b3942946033ac77cf4091524450363
bartmeeus <bart.meeus@student.kdg.be>
2018-03-20 00:43:27 +0100

    deployment url fix

--- server.js
+++ server.js
@@ -11,5 +11,5 @@ app.listen(process.env.PORT || 8080);
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/src/index.html'));
 });
