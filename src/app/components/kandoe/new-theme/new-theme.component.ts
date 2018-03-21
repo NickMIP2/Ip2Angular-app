d0cb3397d691e47d953f72d0910600b82b3efa5a
bartmeeus <bart.meeus@student.kdg.be>
2018-03-21 16:28:34 +0100

    fix new theme tag + register engels

--- src/app/components/kandoe/new-theme/new-theme.component.ts
+++ src/app/components/kandoe/new-theme/new-theme.component.ts
@@ -11,7 +11,7 @@ import {UseridStorage} from '../../../sessionStorage/userid-storage';
   providers: [ThemeService, UseridStorage]
 })
 export class NewThemeComponent implements OnInit {
-  public theme = new Theme(null, '', '',  [''], '');
+  public theme = new Theme(null, '', '',  [], '');
   private userId;
   public tagValue = '';
 
